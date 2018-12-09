3c9feedb4dabf3cef941a81d7f109ebb46f49873
tay <heytay.contact@gmail.com>
2018-12-09 20:33:47 +0100

    validacion contraseña

--- login_registro/registro.php
+++ login_registro/registro.php
@@ -30,6 +30,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if ($resultado !== false) {
             $errores .= '<li>El nombre de usuario ya existe</li>';
         }
+
+        $password = hash('sha512', $password);
+        $password2 = hash('sha512', $password2);  
+        
+        if ($password != $password2) {
+            $errores .= '<li>Las contraseñas no son iguales</li>';
+        }
+    }
+
+    if ($errores == '') {
+        $statement = $conexion->prepare('INSERT INTO usuarios (id,usuario,pass) VALUES (null, :usuario, :pass)');
+        $statement->execute(array(':usuario' => $usuario, ':pass' => $password));
+
+        header('Location: login.php');
     }
 }
 
