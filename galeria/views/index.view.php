9d7e560e881a8ce17be94969548017e7a977e8f0
tay <heytay.contact@gmail.com>
2018-12-10 21:20:31 +0100

    primeras fotos,botones y footer

--- galeria/views/index.view.php
+++ galeria/views/index.view.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
     <title>Galería</title>
 </head>
 <body>
@@ -21,7 +22,34 @@
                     <img src="img/1.jpg" alt="" srcset="">
                 </a>
             </div>
+
+                        <div class="thumb">
+                <a href="">
+                    <img src="img/2.jpg" alt="" srcset="">
+                </a>
+            </div>
+
+                        <div class="thumb">
+                <a href="">
+                    <img src="img/3.jpg" alt="" srcset="">
+                </a>
+            </div>
+
+                        <div class="thumb">
+                <a href="">
+                    <img src="img/4.jpg" alt="" srcset="">
+                </a>
+            </div>
         </div>
     </section>
+
+    <div class="paginacion">
+        <a href="#" class="izquierda"><i class="fa fa-long-arrow-left"></i>Página Anterior</a>
+        <a href="#" class="derecha">Página Siguiente<i class="fa fa-long-arrow-right"></i></a>
+    </div>
+
+    <footer>
+        <div class="copyright">Creado por @drowvoloper</div>
+    </footer>
 </body>
 </html>
