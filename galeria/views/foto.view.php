3ecbcc9ba603586866ba40449935acd3e505a118
tay <heytay.contact@gmail.com>
2018-12-10 21:35:59 +0100

    páginas subir y foto

---
+++ galeria/views/foto.view.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Galería</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor">
+            <h1 class="titulo">Foto: 1.jpg</h1>
+        </div>
+    </header>
+
+    <div class="contenedor">
+        <div class="foto">
+            <img src="img/1.jpg" alt="">
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            <a href="index.php" class="regresar"><i class="fa fa-long-arrow-left"></i>Volver a la galería</a>
+        </div>
+    </div>
+
+    <footer>
+        <div class="copyright">Creado por @drowvoloper</div>
+    </footer>
+</body>
+</html>
---
+++ galeria/views/subir.view.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Galería</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor">
+            <h1 class="titulo">Subir foto</h1>
+        </div>
+    </header>
+
+    <div class="contenedor">
+        <form method="post" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" class="formulario">
+    
+        <label for="foto">Selecciona tu foto</label>
+        <input type="file" name="foto" id="foto">
+
+        <label for="titulo">Título de la foto</label>
+        <input type="text" name="titulo" id="titulo">
+
+        <label for="texto">Descripción</label>
+        <textarea name="texto" id="texto" placeholder="Sobre la foto"></textarea>
+
+        <input type="submit" value="Subir Foto" class="submit">
+    </form>
+    </div>
+
+    <footer>
+        <div class="copyright">Creado por @drowvoloper</div>
+    </footer>
+</body>
+</html>
