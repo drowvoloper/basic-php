9be70658b90ba9c2a2bc98b6f29fe1eadd766a05
tay <heytay.contact@gmail.com>
2018-12-10 17:36:06 +0100

    estructura y estilos css

---
+++ galeria/views/index.view.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Galería</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor">
+            <h1 class="titulo">Mi Galería en PHP y MySQL</h1>
+        </div>
+    </header>
+
+    <section class="fotos">
+        <div class="contenedor">
+            <div class="thumb">
+                <a href="">
+                    <img src="img/1.jpg" alt="" srcset="">
+                </a>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
