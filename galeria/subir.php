a615e915ce1c1febb44574f6ea663661e0307da5
tay <heytay.contact@gmail.com>
2018-12-12 16:35:45 +0100

    subir.php funcionando

--- galeria/subir.php
+++ galeria/subir.php
@@ -11,10 +11,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_FILES)) {
     $check = @getimagesize($_FILES['foto']['tmp_name']);
 
     if($check !== false) {
-        $carpeta_destino = '/opt/lampp/htdocs/proyectos-personales/basic-php/galeria/fotos';
+        $carpeta_destino = 'fotos/';
         $archivo_subido = $carpeta_destino . $_FILES['foto']['name'];
-        echo $archivo_subido;
-        #chown($_FILES['foto']['tmp_name'],'www-data');
+        #echo $archivo_subido;
         move_uploaded_file($_FILES['foto']['tmp_name'], $archivo_subido);
 
         $statement = $conexion->prepare('
@@ -23,11 +22,11 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_FILES)) {
 
         $statement->execute(array(
             ':titulo' => $_POST['titulo'],
-            ':imagen' => $_POST['foto']['name'],
+            ':imagen' => $_FILES['foto']['name'],
             ':texto' => $_POST['texto'] 
         ));
 
-        #header('Location: index.php');
+        header('Location: index.php');
     } else {
         $error = 'El archivo no es una imagen o es demasiado pesado';
     }
