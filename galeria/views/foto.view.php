0134726a3d35bbdd1dcc8c49e92d6afac5ba532e
tay <heytay.contact@gmail.com>
2018-12-12 18:30:08 +0100

    cuasi terminado

--- galeria/views/foto.view.php
+++ galeria/views/foto.view.php
@@ -11,14 +11,21 @@
 <body>
     <header>
         <div class="contenedor">
-            <h1 class="titulo">Foto: 1.jpg</h1>
+            <h1 class="titulo">Foto: 
+                <?php if (!empty($foto['titulo'])) {
+                    echo $foto['titulo'];
+                } else {
+                    echo $foto['imagen'];
+                }
+                ?>
+            </h1>
         </div>
     </header>
 
     <div class="contenedor">
         <div class="foto">
-            <img src="img/1.jpg" alt="">
-            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            <img src="fotos/<?php echo $foto['imagen']; ?>" alt="">
+            <p> <?php echo $foto['texto']; ?></p>
             <a href="index.php" class="regresar"><i class="fa fa-long-arrow-left"></i>Volver a la galería</a>
         </div>
     </div>
--- galeria/views/index.view.php
+++ galeria/views/index.view.php
@@ -17,35 +17,24 @@
 
     <section class="fotos">
         <div class="contenedor">
-            <div class="thumb">
-                <a href="">
-                    <img src="img/1.jpg" alt="" srcset="">
-                </a>
-            </div>
-
-                        <div class="thumb">
-                <a href="">
-                    <img src="img/2.jpg" alt="" srcset="">
-                </a>
-            </div>
-
-                        <div class="thumb">
-                <a href="">
-                    <img src="img/3.jpg" alt="" srcset="">
-                </a>
-            </div>
-
-                        <div class="thumb">
-                <a href="">
-                    <img src="img/4.jpg" alt="" srcset="">
-                </a>
-            </div>
+           
+            <?php foreach($fotos as $foto): ?>
+                <div class="thumb">
+                    <a href="foto.php?id=<?php echo $foto['id']; ?>">
+                        <img src="fotos/<?php echo $foto['imagen']; ?>" alt="">
+                    </a>
+                </div>
+            <?php endforeach ?>
         </div>
     </section>
 
     <div class="paginacion">
-        <a href="#" class="izquierda"><i class="fa fa-long-arrow-left"></i>Página Anterior</a>
-        <a href="#" class="derecha">Página Siguiente<i class="fa fa-long-arrow-right"></i></a>
+        <?php if ($pagina_actual > 1): ?>
+            <a href="index.php?p=<?php echo $pagina_actual -1; ?>" class="izquierda"><i class="fa fa-long-arrow-left"></i>Página Anterior</a>
+        <?php endif ?>
+        <?php if ($total_paginas != $pagina_actual): ?>
+            <a href="index.php?p=<?php echo $pagina_actual +1; ?>" class="derecha">Página Siguiente<i class="fa fa-long-arrow-right"></i></a>
+        <?php endif ?>
     </div>
 
     <footer>
