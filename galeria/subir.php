0e7b880e06e8425b6fdf55293412fc55b268df1d
tay <heytay.contact@gmail.com>
2018-12-12 15:06:46 +0100

    commit gratis

--- galeria/subir.php
+++ galeria/subir.php
@@ -11,9 +11,10 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_FILES)) {
     $check = @getimagesize($_FILES['foto']['tmp_name']);
 
     if($check !== false) {
-        $carpeta_destino = 'fotos/';
+        $carpeta_destino = '/opt/lampp/htdocs/proyectos-personales/basic-php/galeria/fotos';
         $archivo_subido = $carpeta_destino . $_FILES['foto']['name'];
         echo $archivo_subido;
+        #chown($_FILES['foto']['tmp_name'],'www-data');
         move_uploaded_file($_FILES['foto']['tmp_name'], $archivo_subido);
 
         $statement = $conexion->prepare('
@@ -26,7 +27,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_FILES)) {
             ':texto' => $_POST['texto'] 
         ));
 
-        header('Location: index.php');
+        #header('Location: index.php');
     } else {
         $error = 'El archivo no es una imagen o es demasiado pesado';
     }
